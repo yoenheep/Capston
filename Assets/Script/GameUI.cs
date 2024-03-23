5a3996702e567eca90bda8adf83cd95c6dbc6241
yoenheep <yoen3212@naver.com>
2024-03-24 02:22:09 +0900

    hp+dashcooltime

--- Assets/Script/GameUI.cs
+++ Assets/Script/GameUI.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameUI : MonoBehaviour
 {
@@ -24,7 +25,16 @@ public class GameUI : MonoBehaviour
     [SerializeField] private GameObject clearPopup;
     public TextMeshProUGUI clearTimeTxt;
 
-    //[Header("# heart")]
+    [Header("Dash")]
+    [SerializeField] private Image DashCoolTime;
+    private float coolTime = 8f;
+    private float coolTime_max = 8f;
+
+    [Header("# heart")]
+    [SerializeField] private Image hpBar;
+    private float hp_max = 100;
+    private float hp_now;
+    private bool isDamaged = false;
 
     //[Header("# weapon")]
 
@@ -36,6 +46,8 @@ public class GameUI : MonoBehaviour
         overPopup.SetActive(false);
         clearPopup.SetActive(false);
         setPopup.SetActive(false);
+
+        hp_now = 100;
     }
 
     private void Update()
@@ -52,6 +64,24 @@ public class GameUI : MonoBehaviour
                 Continue();
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.D)) // 데쉬 쿨타임 이팩트
+        {
+            if(coolTime < 0|| coolTime == 8f)
+            {
+                coolTime = coolTime_max;
+                StartCoroutine(CoolTimeFunc());
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            clearPopup.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        hp(); // HP 임시키
+        hpBar.fillAmount = hp_now / hp_max; // 캐릭터 hpbar
     }
 
     public void Continue() // 계속하기
@@ -69,20 +99,58 @@ public class GameUI : MonoBehaviour
         setPopup.SetActive(true);
     }
 
-    public void x()
+    public void x() // 설정창 끄기 버튼
     {
         stopBg.SetActive(true);
         setPopup.SetActive(false);
     }
 
-    public void NewGame()
+    public void NewGame() // 새게임
     {
         Time.timeScale = 1;
         SceneManager.LoadScene("Game");
     }
 
-    public void goMain()
+    public void goMain() //a메인가기
     {
         SceneManager.LoadScene("Main");
+    }
+
+    IEnumerator CoolTimeFunc() // 데쉬 쿨타임 이팩트
+    {
+        while (coolTime > 0.0f)
+        {
+            coolTime -= Time.deltaTime;
+            DashCoolTime.fillAmount = coolTime / coolTime_max;
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    void CanDamaged()
+    {
+        isDamaged = false;
+    }
+
+    public void hp()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            hp_now -= 10;
+            isDamaged = true;
+
+            if(hp_now <= 0)
+            {
+                overPopup.SetActive(true);
+                Time.timeScale = 0;
+            }
+
+            Invoke("CanDamaged", 0.4f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            hp_now += 10;
+        }
     }
 }

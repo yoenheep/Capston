669918ab0f1bedbcf9060958b4aeef926c7fd05e
yoenheep <yoen3212@naver.com>
2024-03-28 12:57:49 +0900

    timer+

--- Assets/Script/GameUI.cs
+++ Assets/Script/GameUI.cs
@@ -8,6 +8,11 @@ using Unity.VisualScripting;
 
 public class GameUI : MonoBehaviour
 {
+    [Header("# Time")]
+    [SerializeField] private TextMeshProUGUI TimeTxt;
+    private float timeSecs = 0f;
+    private int timeMin = 0;
+
     [Header("# stop")]
     [SerializeField] private GameObject stopPopup;
     [SerializeField] private GameObject stopBg;
@@ -15,8 +20,11 @@ public class GameUI : MonoBehaviour
 
     [Header("# quiz")]
     [SerializeField] private GameObject quizPopup;
-    public InputField answerField;
+    public TMP_InputField answerField;
     public Image quizImg;
+    [SerializeField] private Image quizTimer;
+    private float quizTimer_max = 20f;
+    private float quizTimer_now;
 
     [Header("# over")]
     [SerializeField] private GameObject overPopup;
@@ -27,8 +35,8 @@ public class GameUI : MonoBehaviour
 
     [Header("Dash")]
     [SerializeField] private Image DashCoolTime;
-    private float coolTime = 8f;
-    private float coolTime_max = 8f;
+    private float coolTime = 2f;
+    private float coolTime_max = 2f;
 
     [Header("# heart")]
     [SerializeField] private Image hpBar;
@@ -36,7 +44,9 @@ public class GameUI : MonoBehaviour
     private float hp_now;
     private bool isDamaged = false;
 
-    //[Header("# weapon")]
+    [Header("# weapon")]
+    [SerializeField] private Image AWeaponImg;
+    [SerializeField] private Image SWeaponImg;
 
     private void Awake()
     {
@@ -48,11 +58,12 @@ public class GameUI : MonoBehaviour
         setPopup.SetActive(false);
 
         hp_now = 100;
+        quizTimer_now = 20f;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape)) // 일시정지
+        if (Input.GetKeyDown(KeyCode.Escape) && quizPopup.activeSelf == false) // 일시정지
         {
             if (stopPopup.activeSelf == false)
             {
@@ -67,23 +78,37 @@ public class GameUI : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.D)) // 데쉬 쿨타임 이팩트
         {
-            if(coolTime < 0|| coolTime == 8f)
+            if(coolTime < 0 || coolTime == coolTime_max)
             {
                 coolTime = coolTime_max;
                 StartCoroutine(CoolTimeFunc());
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && quizPopup.activeSelf == false) // 임시 클리어 키
         {
             clearPopup.SetActive(true);
             Time.timeScale = 0;
+            clearTimeTxt.text = TimeTxt.text;
         }
 
+        time(); // 타임표시
+        quiz(); // quiz 임시키
         hp(); // HP 임시키
         hpBar.fillAmount = hp_now / hp_max; // 캐릭터 hpbar
     }
 
+    public void time()
+    {
+        timeSecs += Time.deltaTime;
+        if(timeSecs >= 60)
+        {
+            timeMin += 1;
+            timeSecs = 0;
+        }
+
+        TimeTxt.text = string.Format("{0:D2}:{1:00}", timeMin, (int)timeSecs);
+    }
     public void Continue() // 계속하기
     {
         if (stopPopup.activeSelf == true)
@@ -134,7 +159,7 @@ public class GameUI : MonoBehaviour
 
     public void hp()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Q) && quizPopup.activeSelf == false)
         {
             hp_now -= 10;
             isDamaged = true;
@@ -151,6 +176,43 @@ public class GameUI : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             hp_now += 10;
+        }
+    }
+
+    IEnumerator quizTimerFunc() // 데쉬 쿨타임 이팩트
+    {
+        while (quizTimer_now > 0.0f && quizPopup.activeSelf == true)
+        {
+            quizTimer_now -= Time.deltaTime;
+            quizTimer.fillAmount = quizTimer_now / quizTimer_max;
+
+             yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public void quiz() //임시 퀴즈창
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // 임시퀴즈몬스터키
+        {
+            answerField.text = "";
+            quizPopup.SetActive(true);
+            StartCoroutine(quizTimerFunc()); 
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (answerField.text == "살려줘")
+            {
+                quizPopup.SetActive(false);
+                quizTimer_now = 20f;
+            }
+        }
+
+        if (quizTimer_now <= 0.0f)
+        {
+            quizPopup.SetActive(false);
+            hp_now -= 10.0f;
+            quizTimer_now = 20f;
         }
     }
 }

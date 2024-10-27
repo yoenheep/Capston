e3a9c5fb05c1bb7351e0a83e41844d7948f60c74
KJI <145641961+VodkatiSoviet@users.noreply.github.com>
2024-10-27 19:55:19 +0900

    Revert "회원가입/로그인 DB 연결"
    This reverts commit 6f96e71315990fb3ffc632c2429404c4321a9cb0.

--- Assets/Script/UI/Main.cs
+++ Assets/Script/UI/Main.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Xml;
-using TMPro;
 
 public class Main : MonoBehaviour
 {
@@ -14,98 +11,6 @@ public class Main : MonoBehaviour
     [SerializeField] private GameObject mainPage;
 
     // Start is called before the first frame update
-
-    [Header("# login")]
-    public TMP_InputField loginIdInput;
-    public TMP_InputField loginPWDInput;
-    public GameObject loginUI;
-
-    [Header("# signup")]
-    public TMP_InputField signupIdInput;
-    public TMP_InputField signupPWDInput;
-    public GameObject signupUI;
-
-    public void CloseUi(GameObject ui)
-    {
-        ui.SetActive(false);
-    }
-
-    public void Signup()
-    {
-        if (!string.IsNullOrEmpty(signupIdInput.text) && !string.IsNullOrEmpty(signupPWDInput.text))
-        {
-            string playerName = signupIdInput.text;
-            int pwd = int.Parse(signupPWDInput.text);
-
-            XmlNodeList user = MySQLConnection.Select("users", $"WHERE ID = '{playerName}'");
-
-            if (user != null)
-            {
-                //이미 존재하는 아이디입니다.
-                Debug.Log("이미 존재하는 아이디입니다.");
-            }
-            else
-            {
-                if (pwd.ToString().Length == 4)
-                {
-                    MySQLConnection.Insert("users", $"'{playerName}', {pwd}");
-                    //회원가입 
-                    Debug.Log("회원가입 성공");
-                }
-                else
-                {
-                    //비밀번호는 4자리입니다. 4일때만 입력가능
-                    Debug.Log("비밀번호는 4자리입니다.");
-                }
-            }
-        }
-        else if (string.IsNullOrEmpty(signupIdInput.text) || (!string.IsNullOrEmpty(signupPWDInput.text)))
-        {
-            //아이디가 비어있을 때
-            Debug.Log("ID를 입력해주세요.");
-        }
-        else if (string.IsNullOrEmpty(signupPWDInput.text))
-        {
-            //비밀번호가 비어있을 때
-            Debug.Log("비밀번호를 입력해주세요");
-        }
-    }
-
-    public void Login()
-    {
-        if (!string.IsNullOrEmpty(loginIdInput.text) && !string.IsNullOrEmpty(loginPWDInput.text))
-        {
-            string playerName = loginIdInput.text;
-            int pwd = int.Parse(loginPWDInput.text);
-
-            XmlNodeList user = MySQLConnection.Select("users", $"WHERE ID = '{playerName}'");
-
-            if (user != null)
-            {
-                if (pwd == int.Parse(user[0]["ID"].InnerText))
-                {
-                    // 게임시작
-                }
-                else
-                {
-                    //OpenUI("alert");
-                    //alertText.text = "비밀번호가 잘못되었습니다";
-                }
-            }
-            else
-            {
-                // 존재하지않은 아이디입니다.
-            }
-        }
-        else if (string.IsNullOrEmpty(loginIdInput.text) || (!string.IsNullOrEmpty(loginPWDInput.text)))
-        {
-            //아이디가 비어있을 때
-        }
-        else if (string.IsNullOrEmpty(loginPWDInput.text))
-        {
-            //비밀번호가 비어있을 때
-        }
-    }
     void Awake()
     {
         mainPage.SetActive(true);
